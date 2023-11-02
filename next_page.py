613916d9775e51ca696499243e7c3b4c69ec97ef
bluestonewhitestone <89286297+bluestonewhitestone@users.noreply.github.com>
2023-11-02 14:04:18 -0700

    Create next_page.py

---
+++ next_page.py
@@ -0,0 +1,109 @@
+import requests
+import json
+import random
+
+response = requests.get("https://the-trivia-api.com/v2/questions")
+
+user_welcome = "Welcome to Quizzical Trivia"
+user_difficulty_easy = "Easy"
+user_difficulty_medium = "Medium"
+user_difficulty_hard = "Hard"
+
+print("""
+       .--.                   .---.
+   .---|__|           .-.     |~~~|
+.--|===|--|_          |_|     |~~~|--.
+|  |===|  |'\     .---!~|  .--|   |--|
+|%%|   |  |.'\    |===| |--|%%|   |  |
+|%%|   |  |\.'\   |   | |__|  |   |  |
+|  |   |  | \  \  |===| |==|  |   |  |
+|  |   |__|  \.'\ |   |_|__|  |~~~|__|
+|  |===|--|   \.'\|===|~|--|%%|~~~|--|
+^--^---'--^    `-'`---^-^--^--^---'--'
+""")
+
+
+print(user_welcome)
+print("")
+print("Choose one:")
+print(user_difficulty_easy, user_difficulty_medium, user_difficulty_hard)
+user_difficulty = input("")
+
+
+if user_difficulty.upper() == user_difficulty_easy.upper() or user_difficulty_medium.upper() or user_difficulty_hard.upper():
+
+    print("You've selected:", user_difficulty.lower())
+else:
+    user_difficulty = input("Please select a difficulty provided")
+
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+
+def select_choice():
+    count = 0
+
+    for index in range(10):
+        if (response.json()[count]["difficulty"]) == user_difficulty.lower():
+            jprint(response.json()[count]["question"]["text"])
+
+            example = (response.json()[count]["incorrectAnswers"])
+            example.append(response.json()[count]["correctAnswer"])
+
+            random.shuffle(example)
+            print(example)
+
+            answer_choice = input("Select an answer")
+
+            answer_input(answer_choice, example, response.json()[count]["correctAnswer"])
+
+            break
+        else:
+            count += 1
+
+
+def answer_input(x, y, z):
+    score = 0
+
+    if x == z:
+        print("CORRECT")
+        score += 1
+        print("Score:", score)
+        select_choice()
+    else:
+        score -= 1
+        print("Score:", score)
+
+
+select_choice()
+
+
+#
+# def possible_answers():
+#     if response.json()[0]["correctAnswer"] ==
+
+
+
+
+
+
+# jprint((response.json()[0]["question"]["text"]))
+#
+# y = (jprint(response.json()[0]["correctAnswer"]))
+#
+#
+# for index in range(3):
+#     jprint(response.json()[0]["incorrectAnswers"][index])
+#
+#
+# x = input("Write the Correct Answer")
+#
+# if str(x) == y:
+#
+#     print("correct")
+
+
